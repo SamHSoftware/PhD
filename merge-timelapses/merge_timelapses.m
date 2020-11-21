1f0dff5d6bab2b7a688109f46336a179c2c85cbc
Samuel Huguet <samhuguet1@gmail.com>
2020-11-21 14:44:53 +0000

    Added commenting of code.

--- merge-timelapses/merge_timelapses.m
+++ merge-timelapses/merge_timelapses.m
@@ -1,3 +1,35 @@
+% merge_timelapses script:
+
+% Author: Sam Huguet
+% e-mail: samhuguet1@gmail.com
+
+% The Yokogawa CV7000S microscope can take timelapse movies of cells.
+% Unfortunately, the imaging process cannot be paused. As a result of this,
+% when the media is changed, one end one imaging protocl then start
+% another. This results in the formation of two seperate folders of images,
+% which, for all intensive purposes, belong to the same timelapse. 
+
+% This script considers the first and second halves of the timelapse, then
+% merges them together into a new folder, located within the directory
+% containing the first half of the images. This new folder is called
+% 'complete-timelapse. 
+
+% The timepoints of images within the second half of the timelapse are
+% corrected, such that they follow on from the maximum timepoint of the
+% first timelapse. 
+
+% The inputs to this script are prompted with automatic dialog boxes, from
+% which you can select the appropriate images/directories. 
+
+% Script input 1: An example image. 
+% Script input 2: The folder corresponding to the first half of the
+% timelapse. 
+% Script input 3: The folder corresponding to the second half of the
+% timelapse. 
+
+% Script output 1: A new folder named 'complete-timelapse', containing all
+% of the images. 
+
 % Reset the Command Window and the Workspace.
 clear;
 clc;
