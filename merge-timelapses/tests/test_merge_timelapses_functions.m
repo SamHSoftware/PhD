f74aade853f0da21980c8ade7db3f1daf2fd7f7c
Samuel Huguet <samhuguet1@gmail.com>
2020-11-28 23:52:05 +0000

    Adding first commit of unit testing script

---
+++ merge-timelapses/tests/test_merge_timelapses_functions.m
@@ -0,0 +1,16 @@
+%% merge_timelapses RUNME.m script:
+
+% Author: Sam Huguet
+% e-mail: samhuguet1@gmail.com
+
+% This script is designed to test the merge_timelapses function. For this
+% script to work, you must download and run this script from this package.
+% This is to preserve path heirarchy, which ensures that code know where to
+% find the provided test input data dn the expected output data. 
+
+% Script input args: None. 
+
+% Script outputs: If the script does not detect any errors, then a
+% statement confirming this will be printed to the command window. If
+% errors are detected, then you will also notified via the command window,
+% alongside a description of the error. 
