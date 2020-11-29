7869dba03c63509e0be33a42910931b80c0f9306
Samuel Huguet <samhuguet1@gmail.com>
2020-11-29 15:04:53 +0000

    Adding code to generate test data for analysis

--- merge-timelapses/tests/test_merge_timelapses_functions.m
+++ merge-timelapses/tests/test_merge_timelapses_functions.m
@@ -14,3 +14,19 @@
 % statement confirming this will be printed to the command window. If
 % errors are detected, then you will also notified via the command window,
 % alongside a description of the error. 
+
+%% Create a new ouput from the test data. 
+
+% Load in the path data to feed into the merge_timelapses function. 
+exampe_image = 'AssayPlate_PerkinElmer_CellCarrier-96_B02_T0001F001L01A01Z01C01.tiff';
+current_directory = matlab.desktop.editor.getActiveFilename;
+first_timelapse_path = fullfile(current_directory(1:end-40), 'data', 'timelapse_1');
+second_timelapse_path = fullfile(current_directory(1:end-40), 'data', 'timelapse_2');
+
+% generate a merged folder of images from the test data. 
+merge_timelapses(example_image, first_timelapse_path, second_timelapse_path);
+complete_timeplapse_path = fullfile(first_timelapse_path, 'complete_timelapse'); 
+
+%% Load in information pertaining to the expected test output data. 
+
+%% Use assertion errors to make necessary checks. 
