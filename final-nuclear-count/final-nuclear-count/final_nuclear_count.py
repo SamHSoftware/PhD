0fb6d726ac284b916ffef4ce1591ed3a8735c129
Samuel Huguet <samhuguet1@gmail.com>
2021-01-26 00:39:12 +0000

    Updating funtion file selection in py file

--- final-nuclear-count/final-nuclear-count/final_nuclear_count.py
+++ final-nuclear-count/final-nuclear-count/final_nuclear_count.py
@@ -22,16 +22,24 @@ def folder_selection_dialog():
 def get_final_nuclei(directory):
      
     # Get a list of the .xlsx files. 
-    files = [file for file in os.listdir(directory) if file.endswith('xlsx')]
+    files = [file for file in os.listdir(directory) if file.endswith('.xlsx')]
+    
+    # Get the processed data folder. 
+    processed_data_directory = os.path.join(directory,'Processed Data')
+    processed_data_file = [file for file in os.listdir(processed_data_directory) if file.endswith('.csv')][0]
+    processed_data_file = os.path.join(processed_data_directory, processed_data_file)
+    
+    # Get the list of files we need to process from the previous folder. 
+    files_to_analyse = pd.read_csv(processed_data_file).iloc[:,14]
     
     # Create a list to hold the data. 
     df = []
-
+    
     # Loop through the individual .xlsx files and extract the 'red' information. 
-    for t in range(len(files)):
+    for t in range(len(files_to_analyse)):
 
         # Construct the file path. 
-        file_t = os.path.join(directory, files[t])
+        file_t = os.path.join(directory, files_to_analyse[t])
         
         # Load in the .xlsx data. 
         data = pd.read_excel(file_t, index_col=None)
@@ -50,4 +58,3 @@ def get_final_nuclei(directory):
     
     # Confirm the code is done. 
     print("Process complete. Your data has been saved (as 'final_nuclear_numbers.csv') within the directory you initially selected.")
-    
