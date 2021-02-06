76c3830b9949c6b8c33945015a16294876645764
Samuel Huguet <samhuguet1@gmail.com>
2021-02-06 18:34:08 +0000

    Adding instructional commenting for function

--- wave-pipeline/wave-pipeline/correctFilledPerimeters.m
+++ wave-pipeline/wave-pipeline/correctFilledPerimeters.m
@@ -1,5 +1,16 @@
-%% Sometimes the function 'correctCrossedColonies.m doesn't catach all crossed colonies. 
-% This script should be used after making a filled perimeter mask, to delete all the weird colony fragments.
+%% correctFilledPerimeters.m
+
+% Author: Sam Huguet 
+% Author e-mail: samhuguet1@gmail.com
+
+% Purpose: Sometimes the function 'correctCrossedColonies.m doesn't catach all crossed colonies. 
+% This script should be used after making a filled perimeter mask, to delete the buggy colony fragments which form.
+
+% Function inputs: 
+% filledPerimeters [int array] --> A filled colony perimeter mask, potentially with colonies erroneously crossing over. 
+
+% Function outputs: 
+% corrected_filledPerimeters [int array] --> A filled colony perimeter mask, now with no colonies crossing over. 
 
 function corrected_filledPerimeters = correctFilledPerimeters(filledPerimeters)
 
