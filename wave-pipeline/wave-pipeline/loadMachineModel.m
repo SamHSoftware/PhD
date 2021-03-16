ee1a1d18b479c5fc46f0d7e41384434494aef9a4
Samuel Huguet <samhuguet1@gmail.com>
2021-03-16 13:49:37 +0000

    Adding commenting to function to return machine model full path

--- wave-pipeline/wave-pipeline/loadMachineModel.m
+++ wave-pipeline/wave-pipeline/loadMachineModel.m
@@ -1,4 +1,22 @@
+%% loadMachineModel.m
 
+% Author: Sam Huguet 
+% Author e-mail: samhuguet1@gmail.com
+
+% Purpose: The function returns the full path to the machine model. 
+
+% Input data requirements: 
+% The model is expected to have been generated using pixelClassifierTrain
+% (written by Marcelo Cicconet, and adapted for use in this pipeline).
+
+% Function inputs: 
+% The trained machine model [struc] --> This is stored within the .mat file
+% you select.
+
+% Function outputs: 
+% The full file path for the machine model. 
+
+%% The function proper. 
 function machineModelFullPath = loadMachineModel
 
 uiwait(msgbox('Select the segmentation model','Sample Image','modal'));
