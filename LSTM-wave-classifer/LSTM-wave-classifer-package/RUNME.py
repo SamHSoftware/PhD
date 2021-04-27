351b1892cf6b1ed40b5c36b1ddda41c33511b1be
Samuel Huguet <samhuguet1@gmail.com>
2021-04-27 12:33:13 +0100

    Updating the RUNME files

--- LSTM-wave-classifer/LSTM-wave-classifer-package/RUNME.py
+++ LSTM-wave-classifer/LSTM-wave-classifer-package/RUNME.py
@@ -1,16 +1,35 @@
 # Import the necessary packages.
 from LSTM_wave_classifier_functions import *
 
-# A function to allow the user to select the folder containing the data.
+# Select the training data.
 # Function inputs args: None. 
 # Function output 1: The path of that the folder selected by the user. 
 training_directory = folder_selection_dialog()
 
-# A function to extract and condense the relevant data. 
+# A function to extract and condense the relevant training data. 
 # Function input arg 1: directory (string) --> The directory to the folder containing the .xlsx data.
 # Function input arg 2: train_or_classify (string) --> Use 'train', when collecting data to train the model. Use 'classify' when collecting data which needs to be classified.
 # Function output 1: df --> The pandas dataframe containing the training information.
 df = get_red_waves(directory,
                    train_or_classify = 'train')
 
+# Train the LSTM model to recognise repetitive sequence data as having 'waves' or not. 
+# Function inputs arg 1: df --> pandas dataframe as provided by the 'get_red_waevs()' function. 
+# Function inputs arg 2: save_plot --> True or False. When True, saves plot to the img folder of the package. 
+# Function inputs arg 3: display_plot --> True or False. When True, displays plot within conole. 
+# Function output 1: The trained model.
+LSTM_model = train_LSTM(df,
+                        save_plot = False,
+                        display_plot = True):
+    
+# Select the folder containing the data which needs to be classified.
+# Function inputs args: None. 
+# Function output 1: The path of that the folder selected by the user. 
+classifiation_directory = folder_selection_dialog()
 
+# A function to extract and condense the relevant training data. 
+# Function input arg 1: directory (string) --> The directory to the folder containing the .xlsx data.
+# Function input arg 2: train_or_classify (string) --> Use 'train', when collecting data to train the model. Use 'classify' when collecting data which needs to be classified.
+# Function output 1: df --> The pandas dataframe containing the training information.
+classification_df = get_red_waves(classifiation_directory,
+                                  train_or_classify = 'classify')
